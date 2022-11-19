650d01e224f94dbc4602404d22ea1d2ffe74e500
CarlosCanales <carloscanales722@gmail.com>
2022-11-19 13:45:02 -0600

    Cambios Models

--- PM2E2GRUPO4/PM2E2GRUPO4/Models/Sitio.cs
+++ PM2E2GRUPO4/PM2E2GRUPO4/Models/Sitio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,24 @@ namespace PM2E2GRUPO4.Models
 {
     public class Sitio
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("latitud")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitud")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("descripcion")]
+        public String Description { get; set; }
+
+        [JsonProperty("fotografia")]
+        public Byte[] Image { get; set; }
+
+        [JsonProperty("audiofile")]
+        public Byte[] AudioFile { get; set; }
+        
     }
 }
+
