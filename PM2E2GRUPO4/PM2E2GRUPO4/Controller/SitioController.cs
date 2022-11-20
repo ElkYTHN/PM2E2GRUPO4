3bbd9200d42ec740deb35544663f6ff11186600f
ElkYTHN <elkinportillo4@gmail.com>
2022-11-20 16:10:51 -0600

    Ultimos

--- PM2E2GRUPO4/PM2E2GRUPO4/Controller/SitioController.cs
+++ PM2E2GRUPO4/PM2E2GRUPO4/Controller/SitioController.cs
@@ -14,87 +14,91 @@ namespace PM2E2GRUPO4.Controller
         private static readonly string URL_SITIOS = "https://elkinhn.online/APIEX/";
         private static HttpClient client = new HttpClient();
 
-        public async static Task<List<Sitio>> GetAllSite()
-        {
-            List<Sitio> listBooks = new List<Sitio>();
-            try
+            public static async Task<List<Sitio>> GetAllSite()
             {
-                var uri = new Uri(URL_SITIOS);
-                var response = await client.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
+                List<Sitio> listBooks = new List<Sitio>();
+                try
                 {
-                    var content = response.Content.ReadAsStringAsync().Result;
-                    listBooks = JsonConvert.DeserializeObject<List<Sitio>>(content);
-                    return listBooks;
+                    var uri = new Uri(URL_SITIOS + "listasitios.php");
+                    var response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = response.Content.ReadAsStringAsync().Result;
+                        listBooks = JsonConvert.DeserializeObject<List<Sitio>>(content);
+                        return listBooks;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                return listBooks;
             }
 
-            return listBooks;
-        }
-
-        public async static Task<bool> DeleteSite(string id)
-        {
-            try
+            public async static Task<bool> DeleteSite(string id)
             {
-                var uri = new Uri(URL_SITIOS + "?id=" + id);
-                var result = await client.DeleteAsync(uri);
-                if (result.IsSuccessStatusCode)
+                try
                 {
-                    return true;
+                    var uri = new Uri(URL_SITIOS + "eliminarsitio.php?id=" + id);
+                    var result = await client.GetAsync(uri);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return false;
-        }
-
-        public async static Task<bool> CreateSite(Sitio sitio)
-        {
-            try
-            {
-                Uri requestUri = new Uri(URL_SITIOS);
-                var jsonObject = JsonConvert.SerializeObject(sitio);
-                var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(requestUri, content);
-                if (response.IsSuccessStatusCode)
+                catch (Exception ex)
                 {
-                    return true;
+                    Console.WriteLine(ex.Message);
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                return false;
             }
 
-            return false;
-        }
-
-
-        public async static Task<bool> UpdateSitio(Sitio sitio)
-        {
-            try
+            public async static Task<bool> CreateSite(Sitio sitio)
             {
-                Uri requestUri = new Uri(URL_SITIOS + "?id=" + sitio.Id);
-                var jsonObject = JsonConvert.SerializeObject(sitio);
-                var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
-                var response = await client.PutAsync(requestUri, content);
-                if (response.IsSuccessStatusCode)
+                try
                 {
-                    return true;
+                    Uri requestUri = new Uri(URL_SITIOS + "crearsitio.php");
+                    var jsonObject = JsonConvert.SerializeObject(sitio);
+                    var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync(requestUri, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return false;
             }
-            catch (Exception ex)
+
+
+            public async static Task<bool> UpdateSitio(Sitio sitio)
             {
-                Console.WriteLine(ex.Message);
+                try
+                {
+                    Uri requestUri = new Uri(URL_SITIOS + "actualizarsitio.php");
+                    var jsonObject = JsonConvert.SerializeObject(sitio);
+                    var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+                    //var response = await client.PutAsync(requestUri, content);
+                    var response = await client.PostAsync(requestUri, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                return false;
             }
-            return false;
         }
+
     }
-}
+
+
